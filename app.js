2aa3ad9acc604c6082788d86db0a8820ed2c67c8
Joe93 <ntuandung93@gmail.com>
2017-02-26 18:55:49 +0000

    Support localhost

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const {MongoClient} = require('mongodb');
 
 const app = express();
 
-app.set('url', process.env.MONGO_URL);
+app.set('url', process.env.MONGO_URL || 'mongodb://localhost:27017/falling-color');
 
-app.set('port', process.env.PORT);
+app.set('port', process.env.PORT || 3000);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
